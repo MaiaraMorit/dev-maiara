141a1dc725974bdc290b1f02ee2b2e657ff04837
MaiaraMorit <maiaramorit@gmail.com>
2025-06-18 21:09:59 -0300

    add apresentation

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,7 +4,14 @@ import styles from './HeroSection.module.css';
 const HeroSection: React.FC = () => {
   return (
     <section className={styles.heroSection}>
-      <div className={styles.rightPanel}></div>
+      <div className={styles.leftPanel}>
+        <p className={styles.hello}>Olá,</p>
+        <p className={styles.name}>Sou<span style={{ marginLeft: '16px' }}></span><span className={styles.highlight}>Maiara</span></p>
+        <p className={styles.role}>Desenvolvedora de software Jr.</p>
+      </div>
+      <div className={styles.rightPanel}>
+        <div className={styles.rightContent}></div>
+      </div>
     </section>
   );
 };
